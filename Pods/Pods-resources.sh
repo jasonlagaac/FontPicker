12f5a93af9dbaab12beb5c4f7da50e0a5187a575
Jason Lagaac <jason@safetyscissors.co>
2013-10-09 20:24:13 +0800

    Added hamcrest and mockito

--- Pods/Pods-resources.sh
+++ Pods/Pods-resources.sh
@@ -18,16 +18,20 @@ install_resource()
     *.framework)
       echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
       mkdir -p "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
-      echo "cp -fpR ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
-      cp -fpR "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      echo "rsync -av ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+      rsync -av "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
       ;;
     *.xcdatamodel)
-      echo xcrun momc "${PODS_ROOT}/$1" ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodel`.mom
-      xcrun momc "${PODS_ROOT}/$1" ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodel`.mom
+      echo "xcrun momc \"${PODS_ROOT}/$1\" \"${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1"`.mom\""
+      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodel`.mom"
       ;;
     *.xcdatamodeld)
-      echo  xcrun momc "${PODS_ROOT}/$1" ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd
-      xcrun momc "${PODS_ROOT}/$1" ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd
+      echo "xcrun momc \"${PODS_ROOT}/$1\" \"${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd\""
+      xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename "$1" .xcdatamodeld`.momd"
+      ;;
+    /*)
+      echo "$1"
+      echo "$1" >> "$RESOURCES_TO_COPY"
       ;;
     *)
       echo "${PODS_ROOT}/$1"
@@ -35,16 +39,19 @@ install_resource()
       ;;
   esac
 }
-install_resource 'FlatUIKit/Resources/Lato-Black.ttf'
-install_resource 'FlatUIKit/Resources/Lato-BlackItalic.ttf'
-install_resource 'FlatUIKit/Resources/Lato-Bold.ttf'
-install_resource 'FlatUIKit/Resources/Lato-BoldItalic.ttf'
-install_resource 'FlatUIKit/Resources/Lato-Hairline.ttf'
-install_resource 'FlatUIKit/Resources/Lato-HairlineItalic.ttf'
-install_resource 'FlatUIKit/Resources/Lato-Italic.ttf'
-install_resource 'FlatUIKit/Resources/Lato-Light.ttf'
-install_resource 'FlatUIKit/Resources/Lato-LightItalic.ttf'
-install_resource 'FlatUIKit/Resources/Lato-Regular.ttf'
+install_resource "FlatUIKit/Resources/Lato-Black.ttf"
+install_resource "FlatUIKit/Resources/Lato-BlackItalic.ttf"
+install_resource "FlatUIKit/Resources/Lato-Bold.ttf"
+install_resource "FlatUIKit/Resources/Lato-BoldItalic.ttf"
+install_resource "FlatUIKit/Resources/Lato-Hairline.ttf"
+install_resource "FlatUIKit/Resources/Lato-HairlineItalic.ttf"
+install_resource "FlatUIKit/Resources/Lato-Italic.ttf"
+install_resource "FlatUIKit/Resources/Lato-Light.ttf"
+install_resource "FlatUIKit/Resources/Lato-LightItalic.ttf"
+install_resource "FlatUIKit/Resources/Lato-Regular.ttf"
 
-rsync -avr --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+if [[ "${ACTION}" == "install" ]]; then
+  rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${INSTALL_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+fi
 rm -f "$RESOURCES_TO_COPY"
